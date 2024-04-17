7fe820fae7c3bceded3cfcc12000faf420f0f34f
seeeeeeong <lsinsung@gmail.com>
2024-04-17 17:07:19 +0900

    refactor: config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -33,19 +33,12 @@ const config: Config = {
     [
       'classic',
       {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-              'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        docs: false,
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-              'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+         routeBasePath: '/'
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -64,12 +57,12 @@ const config: Config = {
         src: 'img/logo.svg',
       },
       items: [
-        {
+        /* {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
           label: 'Tutorial',
-        },
+        }, */
         {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
